1ea5e580280bbddc375af6ba63c75bb892c781c1
iliasaw <62304861+iliasaw@users.noreply.github.com>
2020-12-05 14:01:14 +0300

    Update ttt.py

--- cogs/ttt.py
+++ cogs/ttt.py
@@ -24,6 +24,15 @@ class tictactoe(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.cog_name = ["Ttt"]
+		db = sqlite3.connect("Marry.db")
+		cursor = db.cursor()
+		cursor.execute("""CREATE TABLE IF NOT EXISTS marrys(
+		id1 BIGINT
+		)""")
+		db.commit()
+		db.close()
+		cursor.execute(f"SELECT * FROM marrys WHERE id1='{ctx.message.author.id}'")
+		res = cursor.fetchall()
 
 	@commands.command(
 		aliases=["ttt", "крестики-нолики"],
