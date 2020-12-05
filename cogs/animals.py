26e7bd54d3b1d425181362dc79b281a274c8fb85
iliasaw <62304861+iliasaw@users.noreply.github.com>
2020-12-05 14:52:35 +0300

    Update animals.py

--- cogs/animals.py
+++ cogs/animals.py
@@ -15,30 +15,20 @@ def get_lang(guild, ru_text:str, en_text:str):
 	else:
 		return en_text
 
+res  = 764776986819821569
+	
 class animals(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.cog_name = ["animals"]
-		db = sqlite3.connect("Marry.db")
-		cursor = db.cursor()
-		cursor.execute("""CREATE TABLE IF NOT EXISTS marrys(
-		id1 BIGINT
-		)""")
-		db.commit()
-		db.close()
-		cursor.execute(f"SELECT * FROM marrys WHERE id1='{ctx.message.author.id}'")
-		res = cursor.fetchall()
 
 
 	@commands.command(aliases=["cat", "кот"],
 		description="Показать котика",
 		usage="cat [Ничего]")
 	async def __cat(self, ctx):
-		if ctx.author.id == 465390488963514368:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
-
-		if ctx.author.id == 719605055547768894:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
+		if ctx.author.id == res:
+			return await ctx.send(f"{ctx.author.mention} Вы в чёрном списке")
 		response = requests.get('https://some-random-api.ml/img/cat')
 		jsoninf = json.loads(response.text)
 		url = jsoninf['link']
@@ -50,10 +40,8 @@ class animals(commands.Cog):
 		description="Показать панду",
 		usage="panda [Ничего]")
 	async def __panda(self, ctx):
-		if ctx.author.id == 465390488963514368:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
-		if ctx.author.id == 719605055547768894:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
+		if ctx.author.id == res:
+			return await ctx.send(f"{ctx.author.mention} Вы в чёрном списке")
 		response = requests.get('https://some-random-api.ml/img/panda')
 		jsoninf = json.loads(response.text)
 		url = jsoninf['link']
@@ -65,10 +53,8 @@ class animals(commands.Cog):
 		description="Показать лисичку",
 		usage="fox [Ничего]")
 	async def __fox(self, ctx):
-		if ctx.author.id == 465390488963514368:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
-		if ctx.author.id == 719605055547768894:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
+		if ctx.author.id == res:
+			return await ctx.send(f"{ctx.author.mention} Вы в чёрном списке")
 		response = requests.get('https://some-random-api.ml/img/fox')
 		jsoninf = json.loads(response.text)
 		url = jsoninf['link']
@@ -80,10 +66,8 @@ class animals(commands.Cog):
 		description="Показать коалу",
 		usage="koala [Ничего]")
 	async def __koala(self, ctx):
-		if ctx.author.id == 465390488963514368:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
-		if ctx.author.id == 719605055547768894:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
+		if ctx.author.id == res:
+			return await ctx.send(f"{ctx.author.mention} Вы в чёрном списке")
 		response = requests.get('https://some-random-api.ml/img/koala')
 		jsoninf = json.loads(response.text)
 		url = jsoninf['link']
@@ -95,10 +79,8 @@ class animals(commands.Cog):
 		description="Показать собачку",
 		usage="dog [Ничего]")
 	async def __dog(self, ctx):
-		if ctx.author.id == 719605055547768894:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
-		if ctx.author.id == 465390488963514368:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
+		if ctx.author.id == res:
+			return await ctx.send(f"{ctx.author.mention} Вы в чёрном списке")
 		response = requests.get('https://some-random-api.ml/img/dog')
 		jsoninf = json.loads(response.text)
 		url = jsoninf['link']
@@ -110,10 +92,8 @@ class animals(commands.Cog):
 		description="Показать птичку",
 		usage="bird [Ничего]")
 	async def __bird(self, ctx):
-		if ctx.author.id == 465390488963514368:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
-		if ctx.author.id == 719605055547768894:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
+		if ctx.author.id == res:
+			return await ctx.send(f"{ctx.author.mention} Вы в чёрном списке")
 		response = requests.get('https://some-random-api.ml/img/birb')
 		jsoninf = json.loads(response.text)
 		url = jsoninf['link']
@@ -125,10 +105,8 @@ class animals(commands.Cog):
 		description="Показать красную панду",
 		usage="red-panda [Ничего]")
 	async def __redpanda(self, ctx):
-		if ctx.author.id == 465390488963514368:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
-		if ctx.author.id == 719605055547768894:
-			return await ctx.send(embed = discord.Embed(description = f'**Ты не можешь использовать эту команду**', color=0xa400fc))
+		if ctx.author.id == res:
+			return await ctx.send(f"{ctx.author.mention} Вы в чёрном списке")
 		response = requests.get('https://some-random-api.ml/img/red_panda')
 		jsoninf = json.loads(response.text)
 		url = jsoninf['link']    
